e6773a3550502e032d86f5c9e0d732e753ef44c0
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-01-22 12:28:16 +0100

    basic card setup

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class extends Component {
+    classNames() {
+        if(!this.props['created-by-me']) {
+            return "card"
+        }
+        return "card card--created-by-me"
+    }
+
     render() {
         return (
-            <div className="Card">
-                <div className="title">
-                    {this.props.card.title}
+            <div className={this.classNames()}>
+                <div className="card__title">
+                    {this.props.title}
+                </div>
+                <div className="card__votes">
+                    {this.props.votes}
                 </div>
             </div>
         );
--- test/Card.test.js
+++ test/Card.test.js
@@ -5,9 +5,21 @@ import chai from 'chai';
 chai.should();
 
 describe('ethe card', () => {
-    it('should render a card', () => {
-        const cardState = {title: 'title'};
-        let result = TestUtils.renderIntoDocument(<Card card={cardState}/>);
-        TestUtils.findRenderedDOMComponentWithClass(result, 'title').textContent.should.equal('title');
+    it('should render the title of a card', () => {
+        const result = TestUtils.renderIntoDocument(<Card title="title"/>);
+        TestUtils.findRenderedDOMComponentWithClass(result, 'card__title').textContent.should.equal('title');
+    });
+
+    it('should render the votecount of a card', () => {
+        const result = TestUtils.renderIntoDocument(<Card votes="4"/>);
+        TestUtils.findRenderedDOMComponentWithClass(result, 'card__votes').textContent.should.equal('4');
+    });
+    it('should render the created-by-me class if a card was created by this user', () => {
+        const result = TestUtils.renderIntoDocument(<Card created-by-me="true"/>);
+        TestUtils.findRenderedDOMComponentWithClass(result, 'card').classList.contains('card--created-by-me').should.be.true;
+    });
+    it('should not render the created-by-me class if a card was not created by this user', () => {
+        const result = TestUtils.renderIntoDocument(<Card/>);
+        TestUtils.findRenderedDOMComponentWithClass(result, 'card').classList.contains('card--created-by-me').should.be.false;
     });
 });
