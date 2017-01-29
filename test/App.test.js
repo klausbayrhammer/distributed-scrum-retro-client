2c5b4ad9fd832091e6c1dc8bf5ec92284219589a
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-01-29 20:02:07 +0100

    switches App.test to enzyme full dom rendering

--- test/App.test.js
+++ test/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import App from '../src/App';
 
 describe('New test', function () {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
+        mount(<App/>)
     });
 });
