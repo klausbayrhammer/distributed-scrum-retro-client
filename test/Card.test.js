2c3f88ddb186a8caece3ec8fa9f06cf77127ddcd
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-01-29 19:53:27 +0100

    switched to enzyme tests

--- test/Card.test.js
+++ test/Card.test.js
@@ -1,25 +1,25 @@
-import TestUtils from 'react-addons-test-utils';
 import React from 'react';
+import { shallow } from 'enzyme';
 import Card from '../src/components/Card';
 import chai from 'chai';
 chai.should();
 
 describe('ethe card', () => {
     it('should render the title of a card', () => {
-        const result = TestUtils.renderIntoDocument(<Card title="title"/>);
-        TestUtils.findRenderedDOMComponentWithClass(result, 'card__title').textContent.should.equal('title');
+        const wrapper = shallow(<Card title="title"/>);
+        wrapper.find('.card__title').text().should.equal('title');
     });
 
     it('should render the votecount of a card', () => {
-        const result = TestUtils.renderIntoDocument(<Card votes="4"/>);
-        TestUtils.findRenderedDOMComponentWithClass(result, 'card__votes').textContent.should.equal('4');
+        const wrapper = shallow(<Card votes="4"/>);
+        wrapper.find('.card__votes').text().should.equal('4');
     });
     it('should render the created-by-me class if a card was created by this user', () => {
-        const result = TestUtils.renderIntoDocument(<Card created-by-me="true"/>);
-        TestUtils.findRenderedDOMComponentWithClass(result, 'card').classList.contains('card--created-by-me').should.be.true;
+        const wrapper = shallow(<Card created-by-me="true"/>);
+        wrapper.find('.card').hasClass('card--created-by-me').should.be.true;
     });
     it('should not render the created-by-me class if a card was not created by this user', () => {
-        const result = TestUtils.renderIntoDocument(<Card/>);
-        TestUtils.findRenderedDOMComponentWithClass(result, 'card').classList.contains('card--created-by-me').should.be.false;
+        const wrapper = shallow(<Card/>);
+        wrapper.find('.card').hasClass('card--created-by-me').should.be.false;
     });
 });
