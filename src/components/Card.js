82b707fca289e1ffc31b43d691b4736c150d8676
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-02-01 20:34:34 +0100

    adds buttons for delete cards add/remove votes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 
-export default ({title, votes, createdByMe}) =>
+export default ({title, votes, createdByMe, id, deleteCard, addVote, removeVote} = {}) =>
     <li className={createdByMe ? "card card--created-by-me" : "card"}>
         <div className="card__title">
             {title}
@@ -8,4 +8,7 @@ export default ({title, votes, createdByMe}) =>
         <div className="card__votes">
             {votes}
         </div>
+        <button className="card__delete" onClick={() => deleteCard(id)} />
+        <button className="card__add-vote" onClick={() => addVote(id)} />
+        <button className="card__remove-vote" onClick={() => removeVote(id)} />
     </li>
