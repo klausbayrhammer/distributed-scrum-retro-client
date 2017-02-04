10bb4d541195647332a6f767d6bfd5f7f33b3b95
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-02-04 21:26:17 +0100

    createCard functionality

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ class App extends Component {
         this.state = {columns: [{id:1, title: "Good"}, {id:2, title: "Bad"}, {id: 3, title: "Next actions"}]};
     }
 
+    createCard({columnId, title}) {
+        const card = {title, votes:0};
+        this.state = Object.assign({}, this.state, {columns: this.state.columns.map(column => {
+            if(column.id !== columnId) {
+                return column
+            }
+            return Object.assign({}, column, {cards: [...(column.cards || []), card]})
+        })})
+    }
     render() {
         return (
             <div className="App">
-                <Board columns={this.state.columns}/>
+                <Board columns={this.state.columns} createCard={this.createCard.bind(this)}/>
             </div>
         );
     }
--- test/App.test.js
+++ test/App.test.js
@@ -9,8 +9,13 @@ chai.should();
 describe('App', function () {
     it('should have three empty columns as initial state', () => {
         const wrapper = shallow(<App />);
-        const initialState = [{title:"Good"}, {title:"Bad"}, {title:"Next actions"}];
+        const initialState = [{id:1, title:"Good"}, {id:2, title:"Bad"}, {id:3, title:"Next actions"}];
         wrapper.state('columns').should.eql(initialState);
         wrapper.find(Board).get(0).props.columns.should.eql(initialState)
+    });
+    it('should add a card when the createCard function is invoked', function() {
+        const wrapper = shallow(<App />);
+        wrapper.find(Board).get(0).props.createCard({columnId:1, title:'title'});
+        wrapper.state('columns').should.eql([{id:1, title:"Good", cards: [{title:'title', votes:0}]}, {id:2, title:"Bad"}, {id:3, title:"Next actions"}])
     });
 });
