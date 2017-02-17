c86f1c03beba4916b134b2091be3a35a28173d84
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2017-02-17 18:58:03 +0100

    fixes webpack-dev-server reloading
    see https://webpack.github.io/docs/webpack-dev-server.html
    This modified bundle is served from memory at the relative path specified in publicPath (see API). It will not be written to your configured output directory. Where a bundle already exists at the same URL path, the bundle in memory takes precedence (by default).

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: ['./src/index.js'],
     output: {
         path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
         filename: 'appBundle.js'
     },
     module: {
